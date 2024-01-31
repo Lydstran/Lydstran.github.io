a13b14d0b2421ca7c673fd2d7fd00cd9d59ef96c
Lydstran <Lydia@thestranathans.com>
2024-01-31 22:37:39 +0000

    croatia post

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
-        $('.nav').toggleClass('showing');
-        $('.nav ul').toggleClass('showing');
+        $('nav').toggleClass('showing');
     });
 });
