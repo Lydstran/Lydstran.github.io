4650d395f09069996ac829721fb6cc4ef73d52d8
Lydstran <Lydia@thestranathans.com>
2024-02-05 01:46:55 +0000

    formating edits/hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
+document.getElementById("hamburger-button").addEventListener('click', function() {
+    let element = document.getElementById("hamburger-nav");
+    if (element.style.display === 'none' || window.getComputedStyle(element).display === 'none') {
+        element.style.display = 'flex';
+    } else {
+        element.style.display = 'none';
+    }
+})
+
+
+
+
+/*
 $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
-        $('nav').toggleClass('showing');
+        console.log(document.getElementById("hamburger-nav").attributes);
+        //$('nav').toggleClass('showing');
+        if (document.getElementById("hamburger-nav").hasAttribute("hidden")) {
+            console.log("Hidden");
+        } else {
+            console.log("Showing")
+        }
     });
 });
+*/
